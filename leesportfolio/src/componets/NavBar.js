d42245beb79d5a21530fdc4e001133fac9ecd7cb
soulfulnoise <leespaulding25@gmail.com>
2021-05-26 15:15:34 -0400

    added aboutme tab to navbar

--- leesportfolio/src/componets/NavBar.js
+++ leesportfolio/src/componets/NavBar.js
@@ -6,11 +6,28 @@ function NavBar(props) {
             <li className="nav-item">
                 <a href="#home"
                 onClick={() => props.handlePageChange("Home")}
-                className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}>
+                className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+                >
                     Home
                     </a>
             </li>
+            <li className="nav-item">
+                <a href="#aboutme"
+                onClick={() => props.handlePageChange("Aboutme")}
+                className={props.currentPage === "Aboutme" ? "nav-link active" : "nav-link"}
+                >
+                    Aboutme
+                    </a>
+            </li>
+
+
+
         </ul>
+
+        
+
+
+        
     )
 
 }
